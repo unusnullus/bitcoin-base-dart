d0c9b61c81278359a84833392e7197f8c68eebc0
Maksim Stankevich <maksim.stankevich@innowise.com>
2026-03-11 12:43:02 +0100

    Remove example and unused dependencies.

--- example/lib/services_examples/cross_platform_websocket/cross.dart
+++
@@ -1,5 +0,0 @@
-import 'core.dart';
-
-Future<WebSocketCore> connectSoc(String url, {List<String>? protocols}) =>
-    throw UnsupportedError(
-        'Cannot create a instance without dart:html or dart:io.');
